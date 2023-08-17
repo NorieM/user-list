6a441862aa521774d4814c574dba7f098f55b57f
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2023-08-17 09:03:19 +0100

    Basic CSS for Card and UserList

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "../UI/Card/Card";
+import './UserList.css';
 
 const UserList = (props) => {
   const renderedUsers = props.users.map((user) => {
     return <Card name={user.age} age={user.name} />;
   });
-  return <div>{renderedUsers}</div>;
+  return <div className="userlist">{renderedUsers}</div>;
 };
 
 export default UserList;
